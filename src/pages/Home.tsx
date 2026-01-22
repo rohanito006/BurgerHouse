1394be8e5afacf5fe75f119ff6439a90b9788553
rohanito006 <nyogbarohan@gmail.com>
2026-01-22 15:59:37 +0000

    installation de package

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+
+
+
+function HomePages() {
+
+  return (
+    <>
+        <h1 className="text-4xl text-center mt-10">Bienvenue au Burger House 🍔</h1>
+    </>
+  )
+}
+
+export default HomePages;
---
+++ src/pages/Menu.tsx
@@ -0,0 +1,10 @@
+function MenuPage () {
+
+  return (
+    <>
+        <h1 className="text-4xl text-center mt-10">Voici notre menu 🍔🍟🥤</h1>
+    </>
+  )
+}
+
+export default MenuPage;
